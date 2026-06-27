579f9fa6735f09fbe3b0888fc1e30d545f1ddd82
Hai-Dang Huynh-Lam <22542993+HaiDang2001VN@users.noreply.github.com>
2026-06-27 17:42:17 +0700

    Ajusted

--- vlkge/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""KGE Model Implementations"""
-
-from vlkge.models.transe import TransE
-from vlkge.models.complex import ComplEx
-from vlkge.models.distmult import DistMult
-from vlkge.models.rotate import RotatE
-from vlkge.models.vlkge import VLKGEBase
-
-__all__ = ["VLKGEBase", "TransE", "ComplEx", "DistMult", "RotatE"]
